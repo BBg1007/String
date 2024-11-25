672c8ede95cfb06aade1fbe519896b2599a5579f
BBg1007 <bbgalexson@gmail.com>
2024-11-25 04:24:31 +0300

    Homework complete.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //Task1
+
+        System.out.println("Задача 1");
+        System.out.println();
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName+" "+middleName+" "+firstName;
+        System.out.println("Ф. И. О. сотрудника — "+fullName);
+        System.out.println();
+
+        //Task2
+
+        System.out.println("Задача 2");
+        System.out.println();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — "+fullName.toUpperCase());
+        System.out.println();
+
+        //Task3
+
+        System.out.println("Задача 3");
+        System.out.println();
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — "+fullName);
+
+
     }
 }
